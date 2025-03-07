9fca07a62b04ea524bf7c32010a9b233a42e897d
Mohammed Alshahrani <mmogib@gmail.com>
2025-03-07 21:32:25 +0300

    adde readme

--- src/deeplp/__init__.py
+++ src/deeplp/__init__.py
@@ -1,9 +1,9 @@
 from .main import main
 
 from .train import train
-from .problems import createProblem
+from .problems import *
 
 from .utils import in_notebook
 from .models import *
 
-from .solvers import solve_lp
+from .solvers import *
